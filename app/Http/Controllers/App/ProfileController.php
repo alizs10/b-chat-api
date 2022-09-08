6401ace5d771c720b7904e8bdb7aa7df532ff1db
alizs10 <ali.text77@gmail.com>
2022-09-08 21:29:38 +0430

    fix: update user profile

--- app/Http/Controllers/App/ProfileController.php
+++ app/Http/Controllers/App/ProfileController.php
@@ -4,15 +4,37 @@ namespace App\Http\Controllers\App;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\App\ProfileRequest;
+use App\Http\Services\Image\ImageService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
-    public function update(ProfileRequest $request)
+    public function update(ProfileRequest $request, ImageService $imageService)
     {
         $user = Auth::user();
         $inputs = $request->all();
+
+        if ($request->hasFile('profile_photo')) {
+            if (!empty($user->profile_photo)) {
+                $imageService->deleteImage($user->profile_photo);
+            }
+
+            $imageService->setExclusiveDirectory('user' . DIRECTORY_SEPARATOR . 'avatar');
+            $result = $imageService->save($request->file('profile_photo'));
+
+
+            if (!$result) {
+
+                return response()->json([
+                    'user' => $user,
+                    'status' => false,
+                ]);
+            }
+
+            $inputs['profile_photo'] = $result;
+        }
+        
         $user->update($inputs);
 
         return response()->json([
